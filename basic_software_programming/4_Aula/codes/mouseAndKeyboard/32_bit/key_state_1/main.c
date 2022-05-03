d19ae4387fa46de751fe755597610b39abe91d3f
douglasmatosdev <douglasmatosdev@gmail.com>
2022-05-03 19:43:54 -0300

    get mouse position, button left, middle and right

--- basic_software_programming/4_Aula/codes/mouseAndKeyboard/32_bit/key_state_1/main.c
+++ basic_software_programming/4_Aula/codes/mouseAndKeyboard/32_bit/key_state_1/main.c
@@ -1,58 +1,38 @@
 #include <stdio.h>
-
-#include <stdlib.h>
-
-// #include <windows.h>
-// #include <linux/input.h>
-#include <linux/keyboard.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main()
 {
 
-    while (1)
+    int mouseInfo, bytes;
+    unsigned char data[3];
+
+    const char *mouseFile = "/dev/input/mice";
+
+    mouseInfo = open(mouseFile, O_RDWR);
+    if (mouseInfo == -1)
     {
-        if (getKeyState("KEY_UP"))
-        {
-            printf("Botao da esquerda apertado\n");
-        }
+        printf("ERROR Opening %s\n", mouseFile);
+        return -1;
     }
 
-    /*
+    int left, middle, right;
+    signed char x, y;
     while (1)
     {
+        bytes = read(mouseInfo, data, sizeof(data));
 
-        // Verifica se o botão esquerdo do mouse está pressionado
-
-        if ((GetKeyState(VK_LBUTTON) & 0x80) != 0) //
-
+        if (bytes > 0)
         {
+            left = data[0] & 0x1;
+            right = data[0] & 0x2;
+            middle = data[0] & 0x4;
 
-            Sleep(200);
-
-            printf( "Botao da esquerda apertado\n");
+            x = data[1];
+            y = data[2];
+            printf("x=%d, y=%d, left=%d, middle=%d, right=%d\n", x, y, left, middle, right);
         }
-
-        // Verifica se o botão direito do mouse está pressionado
-
-        if ((GetKeyState(VK_RBUTTON) & 0x80) != 0)
-
-        {
-
-            Sleep(200);
-
-            printf( "Botao da direita apertado\n");
-
-            // Verifica se a tecla A está pressionada
-
-            if ((GetKeyState(65) & 0x80) != 0) // 64 em decimal = 0x41 em hexadecimal
-
-            {
-
-                Sleep(200);
-
-                printf( "Tecla A pressionada\n");
-            }
-        }
-*/
+    }
     return 0;
 }
